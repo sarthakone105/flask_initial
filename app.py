98173638172fc27e8c587ffed0b554088cabe9d4
Sarthak Sharma <sarthaksharma1105@gmail.com>
2024-07-24 21:12:16 +0530

    Created Database in flask

--- app.py
+++ app.py
@@ -1,13 +1,54 @@
-from flask import Flask, render_template, request
-
+from flask import Flask, render_template, request, redirect
+from flask_sqlalchemy import SQLAlchemy
+from flask_scss import Scss
+from datetime import datetime
 
 app = Flask(__name__)
+Scss(app)
 
-@app.route("/")
+app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///database.db"
+db = SQLAlchemy(app)
 
-def landing():
-    return render_template('index.html')
+class MyTask(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    content = db.Column(db.String(100), nullable=False)
+    complete = db.Column(db.Integer, default=0)
+    created = db.Column(db.DateTime, default=datetime.now)
+    
+    def __repr__(self) -> str:
+        return f"Task {self.id}"
+
+
+
+
+@app.route("/", methods=["POST", "GET"])
+def index():
+    # add task
+    if request.method == "POST":
+        current_task = request.form['content']
+        new_task = MyTask(content=current_task)
+        
+        try:
+            db.session.add(new_task)
+            db.session.commit()
+            return redirect("/")
+        
+        except Exception as e:
+            print(f"Error:{e}")
+            return f"Error:{e}"
+    
+    
+    else:
+        tasks = MyTask.query.order_by(MyTask.created).all()
+        return render_template('index.html', task=tasks)
+    
+    
+
+
+
 
 
 if __name__ == "__main__":
-    app.run(debug=True, port=8080)
+    with app.app_context():
+        db.create_all()
+    app.run(debug=True, port=5353)
